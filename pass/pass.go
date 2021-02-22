ca8161283b550a6e6b1d41c53718aaa59a758274
Renato Aguiar <renato@renatoaguiar.net>
2021-02-21 22:38:16 -0800

    Add memory cache to pass backend

--- pass/pass.go
+++ pass/pass.go
@@ -25,7 +25,9 @@ import (
 const PASS_FOLDER = "protonmail-credentials"
 
 // Pass handles secrets using Linux secret-service as a store.
-type Pass struct{}
+type Pass struct {
+	cache map[string]credentials.Credentials
+}
 
 // Ideally these would be stored as members of Pass, but since all of Pass's
 // methods have value receivers, not pointer receivers, and changing that is
@@ -39,11 +41,11 @@ var passInitialized bool
 // CheckInitialized checks whether the password helper can be used. It
 // internally caches and so may be safely called multiple times with no impact
 // on performance, though the first call may take longer.
-func (p Pass) CheckInitialized() bool {
+func (p *Pass) CheckInitialized() bool {
 	return p.checkInitialized() == nil
 }
 
-func (p Pass) checkInitialized() error {
+func (p *Pass) checkInitialized() error {
 	initializationMutex.Lock()
 	defer initializationMutex.Unlock()
 	if passInitialized {
@@ -58,14 +60,14 @@ func (p Pass) checkInitialized() error {
 	return nil
 }
 
-func (p Pass) runPass(stdinContent string, args ...string) (string, error) {
+func (p *Pass) runPass(stdinContent string, args ...string) (string, error) {
 	if err := p.checkInitialized(); err != nil {
 		return "", err
 	}
 	return p.runPassHelper(stdinContent, args...)
 }
 
-func (p Pass) runPassHelper(stdinContent string, args ...string) (string, error) {
+func (p *Pass) runPassHelper(stdinContent string, args ...string) (string, error) {
 	var stdout, stderr bytes.Buffer
 	cmd := exec.Command("pass", args...)
 	cmd.Stdin = strings.NewReader(stdinContent)
@@ -81,8 +83,14 @@ func (p Pass) runPassHelper(stdinContent string, args ...string) (string, error)
 	return strings.TrimRight(stdout.String(), "\n\r"), nil
 }
 
+func New() *Pass {
+	return &Pass{
+		cache: make(map[string]credentials.Credentials),
+	}
+}
+
 // Add adds new credentials to the keychain.
-func (h Pass) Add(creds *credentials.Credentials) error {
+func (h *Pass) Add(creds *credentials.Credentials) error {
 	if creds == nil {
 		return errors.New("missing credentials")
 	}
@@ -90,17 +98,31 @@ func (h Pass) Add(creds *credentials.Credentials) error {
 	encoded := base64.URLEncoding.EncodeToString([]byte(creds.ServerURL))
 
 	_, err := h.runPass(creds.Secret, "insert", "-f", "-m", path.Join(PASS_FOLDER, encoded, creds.Username))
+
+	if err == nil {
+		h.cache[creds.ServerURL] = credentials.Credentials{
+			ServerURL: creds.ServerURL,
+			Username:  creds.Username,
+			Secret:    creds.Secret,
+		}
+	}
+
 	return err
 }
 
 // Delete removes credentials from the store.
-func (h Pass) Delete(serverURL string) error {
+func (h *Pass) Delete(serverURL string) error {
 	if serverURL == "" {
 		return errors.New("missing server url")
 	}
 
 	encoded := base64.URLEncoding.EncodeToString([]byte(serverURL))
 	_, err := h.runPass("", "rm", "-rf", path.Join(PASS_FOLDER, encoded))
+
+	if err == nil {
+		delete(h.cache, serverURL)
+	}
+
 	return err
 }
 
@@ -131,9 +153,13 @@ func listPassDir(args ...string) ([]os.FileInfo, error) {
 }
 
 // Get returns the username and secret to use for a given registry server URL.
-func (h Pass) Get(serverURL string) (string, string, error) {
+func (h *Pass) Get(serverURL string) (string, string, error) {
 	if serverURL == "" {
 		return "", "", errors.New("missing server url")
+	}
+
+	if creds, ok := h.cache[serverURL]; ok {
+		return creds.Username, creds.Secret, nil
 	}
 
 	encoded := base64.URLEncoding.EncodeToString([]byte(serverURL))
@@ -157,11 +183,20 @@ func (h Pass) Get(serverURL string) (string, string, error) {
 
 	actual := strings.TrimSuffix(usernames[0].Name(), ".gpg")
 	secret, err := h.runPass("", "show", path.Join(PASS_FOLDER, encoded, actual))
+
+	if err == nil {
+		h.cache[serverURL] = credentials.Credentials{
+			ServerURL: serverURL,
+			Username:  actual,
+			Secret:    secret,
+		}
+	}
+
 	return actual, secret, err
 }
 
 // List returns the stored URLs and corresponding usernames for a given credentials label
-func (h Pass) List() (map[string]string, error) {
+func (h *Pass) List() (map[string]string, error) {
 	servers, err := listPassDir()
 	if err != nil {
 		return nil, err
